fd9a5a29bc189e5103ced9245c42220f2542b6ac
jahidmbpi <jahid.redmi10@gmail.com>
2025-02-06 21:25:48 +0600

    create navbar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../nabvar/Navbar";
 import Banner from "./Banner";
 import Catagory from "./foodCatagory/Catagory";
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Banner></Banner>
       <Catagory></Catagory>
     </div>
